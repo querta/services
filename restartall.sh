9ddcb10bed5794b12877dd58f3436b224d42e256
Malik Monte <mmonte@mi-d5.msk.21-school.ru>
2021-04-20 20:24:30 +0300

    mysql todo: telegrafconfig on mysql

--- restartall.sh
+++ restartall.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-kubectl delete deploy nginx-deployment
+kubectl delete svc nginx-svc
+kubectl delete deploy nginx-deploy
+kubectl delete svc mysql-svc
+kubectl delete deploy mysql-deploy
+
 docker build -t nginx_image ./nginx/
-kubectl apply -f ./nginx/nginx.yaml
-# minikube addons enable metallb
+kubectl apply -f ./nginx/setup.yaml
+docker build -t mysql_image ./mysql/
+kubectl apply -f ./mysql/setup.yaml
 sleep 3
 kubectl get deploy
 kubectl get pods
--- setup.sh
+++ setup.sh
@@ -3,8 +3,13 @@
 export MINIKUBE_HOME=/goinfre/$USER/  
 minikube start --vm-driver=virtualbox  
 eval $(minikube docker-env)
+# docker pull metallb/speaker:v0.8.2
+# docker pull metallb/controller:v0.8.2
 
 minikube addons enable metallb
 kubectl apply -f metalconfig.yaml 
 docker build -t nginx_image ./nginx/
-kubectl apply -f ./nginx/nginx.yaml
+kubectl apply -f ./nginx/setup.yaml
+
+docker build -t sql_image ./mysql/
+kubectl apply -f ./mysql/setup.yaml
