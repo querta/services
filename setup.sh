4667c26ad343d4620f535d6c3d16981b37117b8e
Malik Monte <mmonte@mi-d5.msk.21-school.ru>
2021-05-18 13:44:29 +0300

    fixed

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ eval $(minikube docker-env)
 minikube addons enable metallb && kubectl apply -f ./srcs/metalconfig.yaml 
 minikube addons enable dashboard
 
-docker build -t influxdb_image ./srcs/influxdb ; kubectl apply -f ./srcs/influxdb/setup.yaml
-docker build -t ftps_image ./srcs/ftps ; kubectl apply -f ./srcs/ftps/setup.yaml
-docker build -t nginx_image ./srcs/nginx ; kubectl apply -f ./srcs/nginx/setup.yaml
-docker build -t mysql_image ./srcs/mysql ; kubectl apply -f ./srcs/mysql/setup.yaml
-docker build -t pma_image ./srcs/pma ; kubectl apply -f ./srcs/pma/setup.yaml
-docker build -t wordpress_image ./srcs/wordpress ; kubectl apply -f ./srcs/wordpress/setup.yaml
-docker build -t grafana_image ./srcs/grafana ; kubectl apply -f ./srcs/grafana/setup.yaml
+docker build -t influxdb_image ./srcs/influxdb && kubectl apply -f ./srcs/influxdb/setup.yaml
+docker build -t ftps_image ./srcs/ftps && kubectl apply -f ./srcs/ftps/setup.yaml
+docker build -t nginx_image ./srcs/nginx && kubectl apply -f ./srcs/nginx/setup.yaml
+docker build -t mysql_image ./srcs/mysql && kubectl apply -f ./srcs/mysql/setup.yaml
+docker build -t pma_image ./srcs/pma && kubectl apply -f ./srcs/pma/setup.yaml
+docker build -t wordpress_image ./srcs/wordpress && kubectl apply -f ./srcs/wordpress/setup.yaml
+docker build -t grafana_image ./srcs/grafana && kubectl apply -f ./srcs/grafana/setup.yaml
